b39526eed5fe4aca48d5316e3a29e36f6d8346d7
Max Efimoff <emv3@ya.ru>
2019-08-28 17:05:45 +0400

    created users page

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,11 +1,64 @@
 <template>
-  <q-page class="flex ">
-    <h5>Users page</h5>
+  <q-page class="flex">
+    <q-list 
+      class="full-width"
+      separator
+      >
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        clickable
+        v-ripple
+        to="/chat"
+      >
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge 
+            :color="user.online ? 'primary' : 'grey'">
+            {{ user.online ? "Online" : "Offline" }}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
   </q-page>
 </template>
 
 <script>
+
+
+
 export default {
+  data () {
+    return {
+      users: [{
+        id: 1,
+        name: 'Max',
+        online: true,
+      }, {
+        id: 2,
+        name: 'Dan',
+        online: true,
+      }, {
+        id: 3,
+        name: 'Alex',
+        online: false,
+      }, {
+        id: 4,
+        name: 'Anna',
+        online: true,
+      }]
+    }
+  }
 }
 </script>
 
